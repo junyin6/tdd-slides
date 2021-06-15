52a231b06431e9c86c960e647175b27b0f4bcd09
Jun Yin <juy103@ucsd.edu>
2021-06-15 00:01:45 -0700

    :wrench: Initial ESLint and Prettier config

--- src/tests/hello.test.js
+++ src/tests/hello.test.js
@@ -1,9 +1,9 @@
-import React from "react";
+import React from 'react';
 
-describe("JSX", () => {
-  it("calls React.createElement", () => {
-    const createElementSpy = jest.spyOn(React, "createElement");
+describe('JSX', () => {
+  it('calls React.createElement', () => {
+    const createElementSpy = jest.spyOn(React, 'createElement');
     <h1>Hi, JSX!</h1>;
-    expect(createElementSpy).toHaveBeenCalledWith("h1", null, "Hi, JSX!");
+    expect(createElementSpy).toHaveBeenCalledWith('h1', null, 'Hi, JSX!');
   });
 });
