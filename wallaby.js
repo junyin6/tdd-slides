5bf898aa85fd2681efe83e1ed71d969f1f5e5e74
Jun Yin <juy103@ucsd.edu>
2021-06-15 00:17:20 -0700

    :wrench: Initial Wallaby setup

---
+++ wallaby.js
@@ -0,0 +1,11 @@
+module.exports = (wallaby) => ({
+  testFramework: 'jest',
+  env: {
+    type: 'node',
+  },
+  tests: ['src/tests/**/*.test.js'],
+  files: ['src/**/*.js', '!**/*.test.js', '**/.*'],
+  compilers: {
+    '**/*.js': wallaby.compilers.babel(),
+  },
+});
