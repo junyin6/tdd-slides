62e437d2bb7843b6f82fcb66fea53103bc134c48
Jun Yin <juy103@ucsd.edu>
2021-06-15 18:19:28 -0700

    :construction: Starting work on CarouselButton

--- src/tests/hello.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-describe('JSX', () => {
-  it('calls React.createElement', () => {
-    const createElementSpy = jest.spyOn(React, 'createElement');
-    <h1>Hi, JSX!</h1>;
-    expect(createElementSpy).toHaveBeenCalledWith('h1', null, 'Hi, JSX!');
-  });
-});
